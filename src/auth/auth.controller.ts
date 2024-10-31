a84c8d158f8201e796b8179c6ed1c8382e34faab
Suteja Kawinanga <sutejakawinanga29@gmail.com>
2024-10-31 23:30:57 +0800

    wip

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,12 +3,19 @@ import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
 import { RefreshTokenDto } from './dto/refresh-tokens.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
+  @ApiResponse({
+    status: 200,
+    description: 'The user profile has been successfully retrieved.',
+  })
+  @ApiOperation({ summary: 'Signup new user' })
   async signup(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
